16d92f79cef0ac6bdfb1fd729fb9af10ac452a7c
chneau <charles63500@gmail.com>
2024-10-06 11:48:52 +0100

    Refactor environment variables and authentication handling

--- env.go
+++ env.go
@@ -8,13 +8,18 @@ import (
 	"strings"
 )
 
-//go:embed aria-ng.html
-var indexHtml string
-var port = getEnvOrDefault("PORT", "3000")
-var externalPort = getEnvOrDefault("EXTERNAL_PORT", port)
-var secret = getEnvOrDefault("SECRET", randString(16))
-var ariaPort = getEnvOrDefault("ARIA_PORT", "6800")
-var ariaDir = getEnvOrDefault("ARIA_DIR", "/data")
+var (
+	//go:embed aria-ng.html
+	indexHtml    string
+	port         = getEnvOrDefault("PORT", "3000")
+	externalPort = getEnvOrDefault("EXTERNAL_PORT", port)
+	secret       = getEnvOrDefault("SECRET", randString(16))
+	ariaPort     = getEnvOrDefault("ARIA_PORT", "6800")
+	ariaDir      = getEnvOrDefault("ARIA_DIR", "/data")
+	username     = getEnvOrDefault("USERNAME", "")
+	password     = getEnvOrDefault("PASSWORD", secret)
+	authSet      = username != "" || password != ""
+)
 
 func getEnvOrDefault(key, defaultValue string) string {
 	value := os.Getenv(key)
@@ -27,6 +32,12 @@ func getEnvOrDefault(key, defaultValue string) string {
 
 func init() {
 	indexHtml = strings.Replace(indexHtml, "6800", externalPort, -1)
-	encodedSecret := base64.StdEncoding.EncodeToString([]byte(secret))
-	indexHtml = strings.Replace(indexHtml, `secret:""`, `secret:"`+encodedSecret+`"`, -1)
+	if authSet {
+		log.Println("Setting rpc secret on index.html since username or password are set")
+		encodedSecret := base64.StdEncoding.EncodeToString([]byte(secret))
+		indexHtml = strings.Replace(indexHtml, `secret:""`, `secret:"`+encodedSecret+`"`, -1)
+	} else {
+		log.Println("Not setting rpc secret on index.html since username and password are not set")
+		log.Println("Confugure the rpc secret in the aria2 settings:", secret)
+	}
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
 )
 
@@ -18,7 +19,13 @@ func main() {
 	go runAriaForever()
 	app := fiber.New()
 	app.Use(compress.New())
-	app.Use(basicauth.New(basicauth.Config{Users: map[string]string{"": secret}}))
+	app.Use(logger.New())
+	if authSet {
+		log.Println("Using basic auth")
+		app.Use(basicauth.New(basicauth.Config{Users: map[string]string{username: password}}))
+	} else {
+		log.Println("Not using basic auth")
+	}
 	app.Get("*", func(c *fiber.Ctx) error {
 		c.Set("Content-Type", "text/html")
 		c.SendString(indexHtml)
